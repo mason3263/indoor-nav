d1ff644272a983e636af7116adbfe4d4d90cdfe7
Mason Erwine <erwine.4@osu.edu>
2023-10-28 19:00:50 +0000

    serve static files

--- app.ts
+++ app.ts
@@ -1,11 +1,19 @@
 import express, { Express, Request, Response } from 'express';
+import path from 'path';
 
 
 const app: Express = express();
 const port = 3000;
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.use('/static', express.static('public'));
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+    if(req.query.id == null)
+        res.send("No ID");
+    else
+        res.send(req.query.id);
 });
 
 app.listen(port, () => {
